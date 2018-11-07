cd6dde7acd13df47cf9a4fbdbba093ee7f7370c2
archonn <waingwaing8@gmail.com>
2018-11-07 11:25:55 +0630

    Blank screens added

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { createBottomTabNavigator } from 'react-navigation';
+
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hii</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
